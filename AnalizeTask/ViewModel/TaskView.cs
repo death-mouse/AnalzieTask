f6741e8d661f1afdb1eb800a4ab0593b481956fb
death-mouse <deamouse@outlook.com>
2015-11-05 14:53:45 +0300

    Дополнения

--- AnalizeTask/ViewModel/TaskView.cs
+++ AnalizeTask/ViewModel/TaskView.cs
@@ -582,15 +582,13 @@ namespace AnalizeTask.View
         private void AnalazeChanged(String _taskId)
         {
             DateTime dateTime = (DateTime)Properties.Settings.Default["LastAnalize"];
-            /*DateTime dateTime = new DateTime();
-            dateTime = DateTime.Parse("31.08.2015 0:38:41");*/
-            
+            if (dateTime == DateTime.Parse("01.01.0001 00:00:00"))
+                dateTime = DateTime.Now;
+
             string xml = browser.GET(string.Format(string.Format("{0}api/TaskLifetime?taskid={1}", Properties.Settings.Default["URL"], _taskId)), Encoding.UTF8);
 
             XDocument xmlDoc = XDocument.Parse(xml);
-            /*IEnumerable<XElement> tracks = from t in xmlDoc.Root.Elements("TaskLifetimes").Elements("TaskLifetime").Where
-                                                (t => t.Element("Date").Value == dateTime.ToString())
-                                                select t;*/
+            
             IEnumerable<XElement> changed = xmlDoc.Root.Descendants("TaskLifetimes").Elements("TaskLifetime").Where(
                                                         t => (DateTime.Parse(t.Element("Date").Value) >= dateTime)).ToList();
             string text="";
